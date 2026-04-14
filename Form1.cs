1509d792566383bafde923f53ee939019b216f87
neurolius <ewgensafonow@gmail.com>
2026-04-14 23:09:13 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace TaskForEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+        }
+    }
+}
